9a7960b85afc7614821af00257e86ca5b5649bce
Niklas Schweizer <dumblexx@gmail.com>
2018-11-27 15:54:35 +0100

    har laggt till quizens alternativ och rubrik mm

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,30 @@
 /* eslint-disable no-unused-vars */
 import style from '../css/style.css';
 /* eslint-enable no-unused-vars */
+
+const getData = async () => {
+  const response = await fetch('http://104.248.143.87:1338/question1');
+  const json = await response.json();
+  console.log(json);
+  question(json.question);
+  alternatives(json.alternatives);
+};
+const question = question => {
+  const questionDiv = document.getElementById('Question');
+  questionDiv.textContent = question;
+};
+const alternatives = answeres => {
+  answeres.forEach(item => {
+    console.log(item);
+    const alternativesDiv = document.getElementById('alternativesDiv');
+    const answere = document.importNode(alternativesDiv.content, true);
+
+    const answereText = answere.getElementById('answereText');
+    answereText.textContent = item;
+
+    const template = document.getElementById('template');
+    template.appendChild(answere);
+    
+  });
+};
+getData();
