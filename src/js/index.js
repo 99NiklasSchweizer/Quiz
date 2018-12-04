83cd493a770e00425dd24c79a665631430d2f63f
Niklas Schweizer <dumblexx@gmail.com>
2018-12-04 15:51:53 +0100

    Kan svara på första fråga nu och få url för nästa fråga

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,15 @@
 /* eslint-disable no-unused-vars */
 import style from '../css/style.css';
 /* eslint-enable no-unused-vars */
+import password from './config';
 
-const getData = async () => {
-  const response = await fetch('http://104.248.143.87:1338/question1');
+const sendFunction = async option => {
+  const response2 = await fetch('http://104.248.143.87:1338/question1', option);
+  const json2 = await response2.json();
+  getData(json2.nextQuestion);
+};
+const getData = async url => {
+  const response = await fetch(url);
   const json = await response.json();
   console.log(json);
   question(json.question);
@@ -19,22 +25,36 @@ const alternatives = answeres => {
     const alternativesDiv = document.getElementById('alternativesDiv');
     const answere = document.importNode(alternativesDiv.content, true);
 
+    const answereInput = answere.getElementById('answereInput');
     const answereText = answere.getElementById('answereText');
     answereText.textContent = item;
+    answereInput.value = item;
 
     const template = document.getElementById('template');
     template.appendChild(answere);
-    const answeres = document.getElementById('answereInput');
   });
   const answereSend = document.getElementById('answereSend');
   answereSend.addEventListener('click', () => {
-    const anwereInput = document.getElementsByClassName('anwereInput');
-    const inputArr = Array.from(anwereInput);
+    console.log('osossoo');
+    const answereInput = document.getElementsByClassName('answereInput');
+    const inputArr = Array.from(answereInput);
     inputArr.forEach(item => {
-      If (answereInput.checked) {
-
+      if (item.checked) {
+        const send = {
+          password,
+          answer: item.value
+        };
+        const sendJSON = JSON.stringify(send);
+        const option = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+          },
+          body: sendJSON
+        };
+        sendFunction(option);
       }
     });
   });
 };
-getData();
+getData('http://104.248.143.87:1338/question1');
