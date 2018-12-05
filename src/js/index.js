db1f69b278b1a7267d2d7c1ba8d98b432a1a3c34
Niklas Schweizer <dumblexx@gmail.com>
2018-12-05 11:41:51 +0100

    Nästan klar, bara mindre delar som slut skärmen och style.

--- src/js/index.js
+++ src/js/index.js
@@ -3,23 +3,81 @@ import style from '../css/style.css';
 /* eslint-enable no-unused-vars */
 import password from './config';
 
-const sendFunction = async option => {
-  const response2 = await fetch('http://104.248.143.87:1338/question1', option);
-  const json2 = await response2.json();
-  getData(json2.nextQuestion);
+const sendFunction = async (option, url) => {
+  if (url) {
+    console.log(url);
+    const response2 = await fetch(url, option);
+    const json2 = await response2.json();
+    if (json2.nextQuestion === null) {
+      console.log('sdfds');
+      quizOver();
+    } else {
+      console.log('asdasdasd');
+      getData(json2.nextQuestion);
+    }
+  }
 };
 const getData = async url => {
-  const response = await fetch(url);
-  const json = await response.json();
-  console.log(json);
-  question(json.question);
-  alternatives(json.alternatives);
+  if (url) {
+    const response = await fetch(url);
+    const json = await response.json();
+    console.log(json);
+    question(json.question);
+    if (json.alternatives) {
+      alternatives(json.alternatives, url);
+    } else {
+      textInput(json.question, url);
+    }
+  }
 };
 const question = question => {
   const questionDiv = document.getElementById('Question');
   questionDiv.textContent = question;
 };
-const alternatives = answeres => {
+const quizOver = () => {
+  const div = document.getElementById('template');
+  while (div.firstChild) {
+    div.removeChild(div.firstChild);
+  }
+  function removeElement(answereSend) {
+    const removeBtn = document.getElementById('anwereSend');
+    removeBtn.parentNode.removeChild(removeBtn);
+  }
+  const text = document.getElementById('Question');
+  text.textContent = 'The Quiz is over!';
+};
+const textInput = (input, url) => {
+  const div = document.getElementById('template');
+  while (div.firstChild) {
+    div.removeChild(div.firstChild);
+  }
+  const inputTemplate = document.getElementById('inputTemplate');
+  const Input = document.importNode(inputTemplate.content.firstElementChild, true);
+  console.log(Input.value);
+  div.appendChild(Input);
+  const answereSend = document.getElementById('answereSend');
+  answereSend.addEventListener('click', () => {
+    const send = {
+      password,
+      answer: Input.value
+    };
+    const sendJSON = JSON.stringify(send);
+    console.log(sendJSON);
+    const option = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: sendJSON
+    };
+    sendFunction(option, url);
+  });
+};
+const alternatives = (answeres, url) => {
+  const div = document.getElementById('template');
+  while (div.firstChild) {
+    div.removeChild(div.firstChild);
+  }
   answeres.forEach(item => {
     console.log(item);
     const alternativesDiv = document.getElementById('alternativesDiv');
@@ -52,7 +110,7 @@ const alternatives = answeres => {
           },
           body: sendJSON
         };
-        sendFunction(option);
+        sendFunction(option, url);
       }
     });
   });
